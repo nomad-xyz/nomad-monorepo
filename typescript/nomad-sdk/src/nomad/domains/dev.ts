7905bd8a8473d76357368c112442ea7e0fc6c122
Conner Swann <2635475+yourbuddyconner@users.noreply.github.com>
2022-01-07 06:03:52 -0800

    sdk: bumped and released nomad-sdk, released nomad-monitor (#9)
    * bumped and released nomad-sdk, released nomad-monitor
    
    * keymaster build script multiplatform
    
    * update nomad-monitor for new deploy
    
    * fixed incorrect contract addresses in sdk, published 0.1.10

--- typescript/nomad-sdk/src/nomad/domains/dev.ts
+++ typescript/nomad-sdk/src/nomad/domains/dev.ts
@@ -42,10 +42,12 @@ export const kovan: NomadDomain = {
   tokenRegistry: 'TODO',
   ethHelper: '0x5Fa9A1B5149c3A85d02a43Bb7Aa5C9a72956b0eB',
   home: '0x5635b4B8Ea091b56A64317A89512ad9564BbcafB',
+  // replicas of remote homes deployed to Kovan
+  // NOT  Kovan replicas deployed to remote chains
   replicas: [
     { 
       domain: 5000, 
-      address: '0x84683c2d02FF9DfC65cc2c18Cf668773b318B224' 
+      address: '0xF90FbAc8F2ddc0BAc21A09494c9FD1b94d3311e2' 
     },
     
   ],
@@ -61,10 +63,12 @@ export const moonbasealpha: NomadDomain = {
   bridgeRouter: '0x7F0Ee81930DdaeDB77cA2FF569Fe0A9963D95627',
   tokenRegistry: 'TODO',
   home: '0x20f63643D5E521E8C1d6D73a2dd09D3C5e752Cbe',
+  // replicas of remote homes deployed to Moonbasealpha
+  // NOT  Moonbasealpha replicas deployed to remote chains
   replicas: [
     {
       domain: 3000,
-      address: '0xF90FbAc8F2ddc0BAc21A09494c9FD1b94d3311e2',
+      address: '0x84683c2d02FF9DfC65cc2c18Cf668773b318B224',
     },
   ],
 };
