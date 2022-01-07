2f99318a0ca9b6694bbd0d8673489e5714aa9dc7
Conner Swann <2635475+yourbuddyconner@users.noreply.github.com>
2022-01-06 19:21:15 -0800

    fixed incorrect contract addresses in sdk, published 0.1.10

--- typescript/nomad-sdk/src/nomad/domains/staging.ts
+++ typescript/nomad-sdk/src/nomad/domains/staging.ts
@@ -10,7 +10,7 @@ export const kovan: NomadDomain = {
   replicas: [
     { 
       domain: 5000, 
-      address: '0x8f8424DC94b4c302984Ab5a03fc4c2d1Ec95DC92' 
+      address: '0x09B6a30de401084e1D355e859061D6C3EB1bC178' 
     },
   ],
 };
@@ -28,7 +28,7 @@ export const moonbasealpha: NomadDomain = {
   replicas: [
     {
       domain: 3000,
-      address: '0x09B6a30de401084e1D355e859061D6C3EB1bC178',
+      address: '0x8f8424DC94b4c302984Ab5a03fc4c2d1Ec95DC92',
     },
   ],
 };
