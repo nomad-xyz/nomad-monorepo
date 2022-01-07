eb23f32251541d6c64e66c2c85c220d54daa55b7
Anna Carroll <anna.s.carroll@gmail.com>
2022-01-07 20:02:39 +0100

    remove comments, easy to add later

--- typescript/nomad-sdk/src/nomad/domains/dev.ts
+++ typescript/nomad-sdk/src/nomad/domains/dev.ts
@@ -1,36 +1,5 @@
 import { NomadDomain } from './domain';
 
-// Note: 
-// Commented out as the current deployment doesn't include rinkeby
-//
-// export const rinkeby: NomadDomain = {
-//   name: 'rinkeby',
-//   paginate: {
-//     blocks: 2000, 
-//     from: 9811702,
-//   },
-//   id: 2000,
-//   bridgeRouter: '0x9EA9754A42d99A41D17Ee4b6330Fc5a9C785E2e9',
-//   tokenRegistry: 'TODO',
-//   ethHelper: '0x4882cbF04c2c825d8FfB91CfF7F748a86B05F3f3',
-//   home: '0x5E52A432958d2eE8B87da0E2875a8d56abdd3000',
-//   replicas: [
-//     { 
-//       domain: 1000, 
-//       address: '0x700C84df52388f0D5d23F8565a2B40D85d0348A3' 
-//     },
-//     {
-//       domain: 3000,
-//       address: '0x07d12F93980661E17ACa9034b15B285b495f4863',
-//     },
-//     {
-//       domain: 5000,
-//       address: '0x6aA7eC78A9B8D15487F6C181f5446239d61592A9',
-//     },
-    
-//   ],
-// };
-
 export const kovan: NomadDomain = {
   name: 'kovan',
   paginate: {
@@ -47,9 +16,8 @@ export const kovan: NomadDomain = {
   replicas: [
     { 
       domain: 5000, 
-      address: '0xF90FbAc8F2ddc0BAc21A09494c9FD1b94d3311e2' 
+      address: '0xF90FbAc8F2ddc0BAc21A09494c9FD1b94d3311e2',
     },
-    
   ],
 };
 
