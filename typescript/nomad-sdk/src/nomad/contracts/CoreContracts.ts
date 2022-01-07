8dd2883b144c525b741a310edf3f49f823a450d8
James Prestwich <10149425+prestwich@users.noreply.github.com>
2022-01-07 15:42:41 -0800

    feature: goverance messages in SDK (#4)
    * feature: goverance messages in SDK
    
    * chore: update governance router addresses in dev&staging sdk

--- typescript/nomad-sdk/src/nomad/contracts/CoreContracts.ts
+++ typescript/nomad-sdk/src/nomad/contracts/CoreContracts.ts
@@ -14,24 +14,28 @@ interface Core {
   id: number;
   home: Address;
   replicas: ReplicaInfo[];
+  governanceRouter: Address;
 }
 
 export class CoreContracts extends Contracts {
   readonly domain;
   readonly _home: Address;
   readonly _replicas: Map<number, InternalReplica>;
+  readonly _governanceRouter: Address;
   private providerOrSigner?: ethers.providers.Provider | ethers.Signer;
 
   constructor(
     domain: number,
     home: Address,
     replicas: ReplicaInfo[],
+    governaceRouter: Address,
     providerOrSigner?: ethers.providers.Provider | ethers.Signer,
   ) {
     super(domain, home, replicas, providerOrSigner);
     this.providerOrSigner = providerOrSigner;
     this.domain = domain;
     this._home = home;
+    this._governanceRouter = governaceRouter;
 
     this._replicas = new Map();
     replicas.forEach((replica) => {
@@ -61,6 +65,16 @@ export class CoreContracts extends Contracts {
     return core.Home__factory.connect(this._home, this.providerOrSigner);
   }
 
+  get governanceRouter(): core.GovernanceRouter {
+    if (!this.providerOrSigner) {
+      throw new Error('No provider or signer. Call `connect` first.');
+    }
+    return core.GovernanceRouter__factory.connect(
+      this._governanceRouter,
+      this.providerOrSigner,
+    );
+  }
+
   connect(providerOrSigner: ethers.providers.Provider | ethers.Signer): void {
     this.providerOrSigner = providerOrSigner;
   }
@@ -77,16 +91,17 @@ export class CoreContracts extends Contracts {
 
     return {
       id: this.domain,
-      home: this.home.address,
+      home: this._home,
       replicas: replicas,
+      governanceRouter: this._governanceRouter,
     };
   }
 
   static fromObject(data: Core, signer?: ethers.Signer): CoreContracts {
-    const { id, home, replicas } = data;
+    const { id, home, replicas, governanceRouter } = data;
     if (!id || !home || !replicas) {
       throw new Error('Missing key');
     }
-    return new CoreContracts(id, home, replicas, signer);
+    return new CoreContracts(id, home, replicas, governanceRouter, signer);
   }
 }
