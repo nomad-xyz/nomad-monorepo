44b0127025fe5830ef0e2737a875227068b5f1f4
ErinHales <mcbridee093@gmail.com>
2022-01-16 15:33:30 -0700

    add: index.ts

--- examples/example-ui/src/utils/index.ts
+++ examples/example-ui/src/utils/index.ts
@@ -1,9 +1,33 @@
-import { networks, TokenMetadata } from '../config'
+import { networks, TokenMetadata, NetworkMetadata } from '../config'
 
-// determines if the token is native to the selected origin network
+/**
+ * determines if the token is native to the selected origin network
+ */
 export function isNativeToken(network: string, token: TokenMetadata): boolean {
-  
-  const nativeToken = networks[network].
-  return nativeToken.name === token.name
+  return token.nativeOnly && token.nativeNetwork === network 
 }
- 
+
+/**
+ * Retrieves network config given a chain ID
+ */
+export function getNetworkByChainID(chainID: number): NetworkMetadata | undefined {
+  for (const network in networks) {
+    if (networks[network].chainID === chainID) {
+      return networks[network]
+    }
+  }
+  // unsupported network
+  console.error(`network not found: ${chainID}`)
+}
+
+/**
+ * Retrieves network config given a domain ID
+ */
+export function getNetworkByDomainID(domainID: number): NetworkMetadata {
+  for (const network in networks) {
+    if (networks[network].domainID === domainID) {
+      return networks[network]
+    }
+  }
+  throw new Error(`network not found: ${domainID}`)
+}
