b6ff00866e895996b0316bdf385650aa3b1d4718
Conner Swann <2635475+yourbuddyconner@users.noreply.github.com>
2022-01-17 12:42:58 -0800

    added development configs for ropsten and shibuya testnets

--- typescript/nomad-deploy/scripts/development/bridge.ts
+++ typescript/nomad-deploy/scripts/development/bridge.ts
@@ -1,6 +1,8 @@
 import {deployBridgesComplete} from '../../src/bridge';
 import * as kovan from '../../config/testnets/kovan';
+import * as ropsten from '../../config/testnets/ropsten';
 import * as moonbasealpha from '../../config/testnets/moonbasealpha';
+import * as shibuya from '../../config/testnets/shibuya';
 import { BridgeDeploy } from '../../src/bridge/BridgeDeploy';
 import { getPathToDeployConfig } from '../../src/verification/readDeployOutput';
 
@@ -13,6 +15,22 @@ const moonBaseAlphaDeploy = new BridgeDeploy(
   path,
 );
 
-const kovanDeploy = new BridgeDeploy(kovan.chain, kovan.bridgeConfig, path);
+const shibuyaDeploy = new BridgeDeploy(
+  shibuya.chain, 
+  shibuya.bridgeConfig, 
+  path
+);
 
-deployBridgesComplete([kovanDeploy, moonBaseAlphaDeploy]);
+const kovanDeploy = new BridgeDeploy(
+  kovan.chain, 
+  kovan.bridgeConfig, 
+  path
+);
+
+const ropstenDeploy = new BridgeDeploy(
+  ropsten.chain, 
+  ropsten.bridgeConfig, 
+  path
+);
+
+deployBridgesComplete([kovanDeploy, ropstenDeploy, shibuyaDeploy, moonBaseAlphaDeploy]);
--- typescript/nomad-deploy/scripts/development/core.ts
+++ typescript/nomad-deploy/scripts/development/core.ts
@@ -1,10 +1,21 @@
 import {deployComplete} from '../../src/core';
 import * as kovan from '../../config/testnets/kovan';
+import * as ropsten from '../../config/testnets/ropsten';
 import * as moonbasealpha from '../../config/testnets/moonbasealpha';
+import * as shibuya from '../../config/testnets/shibuya';
 import { CoreDeploy } from '../../src/core/CoreDeploy';
 
 let kovanConfig = kovan.devConfig;
-const kovanDeploy = new CoreDeploy(kovan.chain, kovanConfig);
+const kovanDeploy = new CoreDeploy(
+  kovan.chain, 
+  kovanConfig
+);
+
+let ropstenConfig = ropsten.devConfig;
+const ropstenDeploy = new CoreDeploy(
+  ropsten.chain, 
+  ropstenConfig
+);
 
 let moonbaseAlphaConfig = moonbasealpha.devConfig;
 const moonbaseAlphaDeploy = new CoreDeploy(
@@ -12,4 +23,10 @@ const moonbaseAlphaDeploy = new CoreDeploy(
   moonbaseAlphaConfig,
 );
 
-deployComplete([kovanDeploy, moonbaseAlphaDeploy]);
+let shibuyaConfig = moonbasealpha.devConfig;
+const shibuyaDeploy = new CoreDeploy(
+  shibuya.chain,
+  shibuyaConfig,
+);
+
+deployComplete([kovanDeploy, ropstenDeploy, moonbaseAlphaDeploy, shibuyaDeploy]);
