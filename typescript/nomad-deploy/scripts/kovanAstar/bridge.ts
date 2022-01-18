31c5871451bdd710239af80ba21a4927a703eea6
Luke Tchang <ltchang@stanford.edu>
2022-01-18 12:05:46 -0800

    adds bridge and core configs to /rust/config

--- typescript/nomad-deploy/scripts/kovanAstar/bridge.ts
+++ typescript/nomad-deploy/scripts/kovanAstar/bridge.ts
@@ -2,10 +2,10 @@ import { deployBridgesComplete } from '../../src/bridge';
 import * as kovan from '../../config/testnets/kovan';
 import * as astar from '../../config/mainnets/astar';
 import { BridgeDeploy } from '../../src/bridge/BridgeDeploy';
-import { getPathToDeployConfig } from '../../src/verification/readDeployOutput';
+// import { getPathToDeployConfig } from '../../src/verification/readDeployOutput';
 
 // get the path to the latest core system deploy
-const path = getPathToDeployConfig('dev');
+const path = '../../rust/config/kovanAstar';
 
 const kovanDeploy = new BridgeDeploy(kovan.chain, kovan.bridgeConfig, path);
 
