cc62612deeca5f04fe7dfe3d0583f0c85fc8c500
Luke Tchang <ltchang@stanford.edu>
2022-01-18 09:36:27 -0800

    fix: remove moonbase alpha from test deploy

--- typescript/nomad-deploy/scripts/stagingWithAstar/bridge.ts
+++ typescript/nomad-deploy/scripts/stagingWithAstar/bridge.ts
@@ -1,6 +1,5 @@
 import { deployBridgesComplete } from '../../src/bridge';
 import * as kovan from '../../config/testnets/kovan';
-import * as moonbasealpha from '../../config/testnets/moonbasealpha';
 import * as astar from '../../config/mainnets/astar';
 import { BridgeDeploy } from '../../src/bridge/BridgeDeploy';
 import { getPathToDeployConfig } from '../../src/verification/readDeployOutput';
@@ -10,12 +9,6 @@ const path = getPathToDeployConfig('dev');
 
 const kovanDeploy = new BridgeDeploy(kovan.chain, kovan.bridgeConfig, path);
 
-const moonBaseAlphaDeploy = new BridgeDeploy(
-  moonbasealpha.chain,
-  moonbasealpha.bridgeConfig,
-  path,
-);
-
 const astarDeploy = new BridgeDeploy(astar.chain, astar.bridgeConfig, path);
 
-deployBridgesComplete([kovanDeploy, moonBaseAlphaDeploy, astarDeploy]);
+deployBridgesComplete([kovanDeploy, astarDeploy]);
--- typescript/nomad-deploy/scripts/stagingWithAstar/core.ts
+++ typescript/nomad-deploy/scripts/stagingWithAstar/core.ts
@@ -1,19 +1,12 @@
 import { deployComplete } from '../../src/core';
 import * as kovan from '../../config/testnets/kovan';
-import * as moonbasealpha from '../../config/testnets/moonbasealpha';
 import * as astar from '../../config/mainnets/astar';
 import { CoreDeploy } from '../../src/core/CoreDeploy';
 
 let kovanConfig = kovan.stagingConfig;
 const kovanDeploy = new CoreDeploy(kovan.chain, kovanConfig);
 
-let moonbaseAlphaConfig = moonbasealpha.stagingConfig;
-const moonbaseAlphaDeploy = new CoreDeploy(
-  moonbasealpha.chain,
-  moonbaseAlphaConfig,
-);
-
 let astarConfig = astar.stagingConfig;
 const astarDeploy = new CoreDeploy(astar.chain, astarConfig);
 
-deployComplete([kovanDeploy, moonbaseAlphaDeploy, astarDeploy]);
+deployComplete([kovanDeploy, astarDeploy]);
