3f9288b13d487319e523e5216b241fc9f4bcbfa1
Anna Carroll <anna.s.carroll@gmail.com>
2022-01-20 13:49:25 +0100

    switch recovery manager to deployer key

--- typescript/nomad-deploy/config/testnets/moonbasealpha.ts
+++ typescript/nomad-deploy/config/testnets/moonbasealpha.ts
@@ -26,7 +26,7 @@ export const devConfig: CoreConfig = {
   environment: 'dev',
   updater: '0x4177372FD9581ceb2367e0Ce84adC5DAD9DF8D55',
   watchers: ['0x20aC2FD664bA5406A7262967C34107e708dCb18E'],
-  recoveryManager: '0x24F6c874F56533d9a1422e85e5C7A806ED11c036',
+  recoveryManager: '0xa4849f1D96B26066f9C631FCdc8F1457D27Fb5EC',
   optimisticSeconds: 10,
   recoveryTimelock: 180, // 3 minutes
   processGas: 850_000,
@@ -37,7 +37,7 @@ export const stagingConfig: CoreConfig = {
   environment: 'staging',
   updater: '0xD39dd43eFDD867939A2F070469cB3e1252827466',
   watchers: ['0x1795f9A0a6853D6328241e9Dc37203cA545C3b79'],
-  recoveryManager: '0x24F6c874F56533d9a1422e85e5C7A806ED11c036',
+  recoveryManager: '0xa4849f1D96B26066f9C631FCdc8F1457D27Fb5EC',
   optimisticSeconds: 60 * 30, // 30 minutes
   recoveryTimelock: 180, // 3 minutes
   processGas: 850_000,
--- typescript/nomad-deploy/config/testnets/rinkeby.ts
+++ typescript/nomad-deploy/config/testnets/rinkeby.ts
@@ -32,7 +32,7 @@ export const devConfig: CoreConfig = {
   environment: 'dev',
   updater: '0x4177372FD9581ceb2367e0Ce84adC5DAD9DF8D55',
   watchers: ['0x20aC2FD664bA5406A7262967C34107e708dCb18E'],
-  recoveryManager: '0x24F6c874F56533d9a1422e85e5C7A806ED11c036',
+  recoveryManager: '0xa4849f1D96B26066f9C631FCdc8F1457D27Fb5EC',
   governor: {
     domain: chainJson.domain,
     address: '0xa4849f1D96B26066f9C631FCdc8F1457D27Fb5EC',
@@ -47,7 +47,7 @@ export const stagingConfig: CoreConfig = {
   environment: 'staging',
   updater: '0x201dd86063Dc251cA5a576d1b7365C38e5fB4CD5',
   watchers: ['0x22B2855635154Baa41C306BcA979C8c9a077A180'],
-  recoveryManager: '0x24F6c874F56533d9a1422e85e5C7A806ED11c036',
+  recoveryManager: '0x812c096810e8eFAA73efF841B601Ab82a7be9aB0', // rinkeby gnosis safe - 1/1 signers, signer = deployer key (0xa4849f1D96B26066f9C631FCdc8F1457D27Fb5EC)
   governor: {
     domain: chainJson.domain,
     address: '0x812c096810e8eFAA73efF841B601Ab82a7be9aB0', // rinkeby gnosis safe - 1/1 signers, signer = deployer key (0xa4849f1D96B26066f9C631FCdc8F1457D27Fb5EC)
