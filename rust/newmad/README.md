17ce957870fbb7dd3f73ba7aad8662bfcd398a99
James <james@prestwi.ch>
2022-01-22 11:58:46 -0700

    chore: add README

---
+++ rust/newmad/README.md
@@ -0,0 +1,8 @@
+TODOs
+- [ ] DB & persistence strategy
+- [ ] instantiating RunningSystem from Settings
+- [ ] implementations of poller for Ethereum
+- [ ] implementations of reconciliation for Ethereum
+- [ ] implementations of Logic for each agent
+- [ ] instantiating Logic from Settings
+
--- rust/newmad/src/actors/mod.rs
+++ rust/newmad/src/actors/mod.rs
@@ -1,15 +1,17 @@
 use std::{collections::HashMap, thread::JoinHandle};
 
+use crate::error::Result;
+
 pub mod models;
 pub mod observer;
 pub mod reconciliation;
 
 pub struct RunningSystem {
-    home_observer: JoinHandle<()>,
-    replica_observers: HashMap<u32, JoinHandle<()>>,
+    home_observer: JoinHandle<Result<()>>,
+    replica_observers: HashMap<u32, JoinHandle<Result<()>>>,
 
-    core: JoinHandle<()>,
+    core: JoinHandle<Result<()>>,
 
-    home_reconciliation: JoinHandle<()>,
-    replica_reconciliations: HashMap<u32, JoinHandle<()>>,
+    home_reconciliation: JoinHandle<Result<()>>,
+    replica_reconciliations: HashMap<u32, JoinHandle<Result<()>>>,
 }
