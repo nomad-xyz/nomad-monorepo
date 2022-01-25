237ffce54f498ae8d249a1290ebbc098386e0db5
Daniil Naumetc <daniil.naumetc@gmail.com>
2022-01-25 21:31:39 +0100

    chore: ethereup rpc provider

--- tools/indexer/core/index.ts
+++ tools/indexer/core/index.ts
@@ -8,15 +8,14 @@ import { DB } from "./db";
 import Logger from "bunyan";
 dotenv.config({});
 
-const infuraKey = process.env.INFURA_KEY!;
+const ethereumRPC = process.env.ETHEREUM_RPC!;
 const moonbeamRPC = process.env.MOONBEAM_RPC!;
 
 export async function run(db: DB, environment: string, logger: Logger) {
   const ctx = mainnet;
 
   const ethereumId = ctx.mustGetDomain("ethereum").id;
-  const infura = new ethers.providers.InfuraProvider("homestead", infuraKey);
-  ctx.registerProvider(ethereumId, infura);
+  ctx.registerRpcProvider(ethereumId, ethereumRPC);
 
   const moonbeamId = ctx.mustGetDomain("moonbeam").id;
   ctx.registerRpcProvider(moonbeamId, moonbeamRPC);
