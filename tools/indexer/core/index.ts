da03c8ed86f0a41916f8238df57f18aeb6b5fe17
Daniil Naumetc <daniil.naumetc@gmail.com>
2022-01-26 12:03:30 +0100

    chore: env handling

--- tools/indexer/core/index.ts
+++ tools/indexer/core/index.ts
@@ -1,24 +1,30 @@
-import { mainnet } from "@nomad-xyz/sdk/dist";
+import { mainnet, staging, dev, NomadContext } from "@nomad-xyz/sdk/dist";
 import { Processor } from "./consumer";
 import { Orchestrator } from "./orchestrator";
 import * as dotenv from "dotenv";
-import { ethers } from "ethers";
 import { IndexerCollector } from "./metrics";
 import { DB } from "./db";
 import Logger from "bunyan";
 dotenv.config({});
 
-const ethereumRPC = process.env.ETHEREUM_RPC!;
-const moonbeamRPC = process.env.MOONBEAM_RPC!;
-
 export async function run(db: DB, environment: string, logger: Logger) {
-  const ctx = mainnet;
+  let ctx: NomadContext;
+  if (environment === 'prod') {
+    ctx = mainnet
+  } else if (environment === 'staging') {
+    ctx = staging;
+  } else {
+    ctx = dev;
+  }
 
-  const ethereumId = ctx.mustGetDomain("ethereum").id;
-  ctx.registerRpcProvider(ethereumId, ethereumRPC);
+  ctx.domainNumbers.forEach(domain => {
+    const name = ctx.mustGetDomain(domain).name.toUpperCase();
+    const rpcEnvKey = `${name}_RPC`;
+    const rpc = process.env[rpcEnvKey];
+    if (!rpc) throw new Error(`RPC url for domain ${domain} is empty. Please provide as '${rpcEnvKey}=http://...' ENV variable`)
 
-  const moonbeamId = ctx.mustGetDomain("moonbeam").id;
-  ctx.registerRpcProvider(moonbeamId, moonbeamRPC);
+    ctx.registerRpcProvider(domain, rpc);
+  })
 
   const c = new Processor(db, logger);
   const m = new IndexerCollector(environment, logger);
@@ -31,7 +37,7 @@ export async function run(db: DB, environment: string, logger: Logger) {
     logger,
     db
   );
-  await m.startServer(3000);
+  m.startServer(3000);
   await o.init();
   await o.startConsuming();
 }
