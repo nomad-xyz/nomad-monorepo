4503557ba6e0d08e58c63302aca617eb8a18c71d
Daniil Naumetc <daniil.naumetc@gmail.com>
2022-01-26 19:28:45 +0100

    chore: reorg

--- tools/local-environment/tests/addChainCase.ts
+++ tools/local-environment/tests/addChainCase.ts
@@ -2,77 +2,7 @@ import { LocalNetwork, Nomad, Key, utils, Network } from "../src";
 import fs from "fs";
 import { getCustomToken } from "./utils/token/deployERC20";
 import { getRandomTokenAmount, sleep } from "../src/utils";
-import { sendTokensAndConfirm } from "./common";
-
-async function setup() {
-  const tom = new LocalNetwork("tom", 1000, "http://localhost:9545");
-  const jerry = new LocalNetwork("jerry", 2000, "http://localhost:9546");
-
-  const tomActor = new Key();
-  const jerryActor = new Key();
-
-  const t = utils.generateDefaultKeys();
-  const j = utils.generateDefaultKeys();
-
-  tom.addKeys(
-    tomActor,
-    t.updater,
-    t.watcher,
-    t.deployer,
-    t.signer.base,
-    t.signer.updater,
-    t.signer.watcher,
-    t.signer.relayer,
-    t.signer.processor
-  );
-  jerry.addKeys(
-    jerryActor,
-    j.updater,
-    j.watcher,
-    j.deployer,
-    j.signer.base,
-    j.signer.updater,
-    j.signer.watcher,
-    j.signer.relayer,
-    j.signer.processor
-  );
-
-  await Promise.all([tom.up(), jerry.up()]);
-
-  const n = new Nomad(tom);
-  n.addNetwork(jerry);
-
-  n.setUpdater(jerry, j.updater); // Need for an update like updater
-  n.setWatcher(jerry, j.watcher); // Need for the watcher
-  n.setDeployer(jerry, j.deployer); // Need to deploy all
-  n.setSigner(jerry, j.signer.base); // Need for home.dispatch
-  n.setSigner(jerry, j.signer.updater, "updater"); // Need for home.dispatch
-  n.setSigner(jerry, j.signer.relayer, "relayer"); // Need for home.dispatch
-  n.setSigner(jerry, j.signer.watcher, "watcher"); // Need for home.dispatch
-  n.setSigner(jerry, j.signer.processor, "processor"); // Need for home.dispatch
-
-  n.setUpdater(tom, t.updater); // Need for an update like updater
-  n.setWatcher(tom, t.watcher); // Need for the watcher
-  n.setDeployer(tom, t.deployer); // Need to deploy all
-  n.setSigner(tom, t.signer.base); // Need for home.dispatch
-  n.setSigner(tom, t.signer.updater, "updater"); // Need for home.dispatch
-  n.setSigner(tom, t.signer.relayer, "relayer"); // Need for home.dispatch
-  n.setSigner(tom, t.signer.watcher, "watcher"); // Need for home.dispatch
-  n.setSigner(tom, t.signer.processor, "processor"); // Need for home.dispatch
-
-  await n.deploy({ injectSigners: true });
-  await n.startAgents(["updater", "relayer", "processor"]);
-
-  fs.writeFileSync("/tmp/nomad.json", JSON.stringify(n.toObject()));
-
-  return {
-    tom,
-    jerry,
-    tomActor,
-    jerryActor,
-    n,
-  };
-}
+import { sendTokensAndConfirm, setupTwo } from "./common";
 
 async function setupDaffy(n: Nomad) {
   const daffy = new LocalNetwork("daffy", 3000, "http://localhost:9547");
@@ -110,9 +40,6 @@ async function setupDaffy(n: Nomad) {
   // there are new chains to be deployed. Here it will
   // incrementally deploy the "daffy" chain
   await n.deploy({ injectSigners: true });
-
-  await n.stopAllAgents(true);
-  await n.startAgents(["updater", "relayer", "processor"]);
 
   fs.writeFileSync("/tmp/nomad.json", JSON.stringify(n.toObject()));
 
@@ -300,13 +227,18 @@ async function teardown(n: Nomad) {
 
 (async () => {
   // Normally setup and deploy 2 local networks
-  const { tom, jerry, tomActor, jerryActor, n } = await setup();
+  const { tom, jerry, tomActor, jerryActor, n } = await setupTwo();
+  await n.startAgents(["updater", "relayer", "processor"]);
+
   console.log(`Tom and Jerry setup complete`);
 
   let success = false;
   try {
     // Perform incremental deploy of new network daffy
     const { daffy, daffyActor } = await setupDaffy(n);
+    await n.stopAllAgents(true);
+    await n.startAgents(["updater", "relayer", "processor"]);
+    
     console.log(`Daffy setup complete`);
 
     await sendTokensHubAndSpoke(
