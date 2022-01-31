575c003981b4a0a86d2795982afd492994f68835
Luke Tchang <ltchang@stanford.edu>
2022-01-30 22:25:25 -0800

    e2e test uses try/catch as expected

--- tools/local-environment/tests/sdkFailedHome.ts
+++ tools/local-environment/tests/sdkFailedHome.ts
@@ -1,8 +1,8 @@
-import { ethers } from "ethers";
-import { Key, utils } from "../src";
+import { ethers } from 'ethers';
+import { Key, utils } from '../src';
 
-import { setupTwo } from "./common";
-import { TokenIdentifier } from '@nomad-xyz/sdk/nomad'
+import { setupTwo } from './common';
+import { TokenIdentifier, FailedHomeError } from '@nomad-xyz/sdk/nomad';
 
 async function testSdkFailedHome() {
   let success = false;
@@ -25,7 +25,7 @@ async function testSdkFailedHome() {
       await tomHome.dispatch(
         jerry.domain,
         ethers.utils.hexZeroPad(address, 32),
-        Buffer.from(`01234567890123456789012345678`, "utf8")
+        Buffer.from(`01234567890123456789012345678`, 'utf8'),
       )
     ).wait();
 
@@ -36,11 +36,11 @@ async function testSdkFailedHome() {
     const updater = await n.getUpdater(tom);
 
     const fraudRoot =
-      "0x8bae0a4ab4517a16816ef67120f0e3350d595e014158ba72c3626d8c66b67e53";
+      '0x8bae0a4ab4517a16816ef67120f0e3350d595e014158ba72c3626d8c66b67e53';
 
     const { signature: improperSignature } = await updater.signUpdate(
       committedRoot,
-      fraudRoot
+      fraudRoot,
     );
 
     // Submit fraud to tom home
@@ -50,33 +50,39 @@ async function testSdkFailedHome() {
 
     const state = await tomHome.state();
     if (state !== 2) {
-      throw new Error("Tom home not failed after improper update!")
+      throw new Error('Tom home not failed after improper update!');
     }
 
     console.log(`Submitted fraud update to tom home!`);
 
-    
     const token: TokenIdentifier = {
       domain: tom.domain,
-      id: "0x111111",
+      id: '0x111111',
     };
 
-    // Try to send token through SDK to trigger tom home check
-    const res = await nomadContext.send(
-      tom.name,
-      jerry.name,
-      token,
-      100,
-      "0x222222222",
-      false,
-      {
-        gasLimit: 10000000,
+    // Try to send token through SDK and expect error after preflight check
+    try {
+      await nomadContext.send(
+        tom.name,
+        jerry.name,
+        token,
+        100,
+        '0x222222222',
+        false,
+        {
+          gasLimit: 10000000,
+        },
+      );
+    } catch (e) {
+      // Expect failed home error
+      if (!(e instanceof FailedHomeError)) {
+        throw new Error('Error was not of type FailedHomeError');
       }
-    );
 
-    // Expect blacklist to contain failed tom home
-    if (!nomadContext.blacklist().has(tom.domain)) {
-      throw new Error("SDK did not black list he failed tom home!")
+      // Expect blacklist to be populated
+      if (!nomadContext.blacklist().has(tom.domain)) {
+        throw new Error('SDK did not black list he failed tom home!');
+      }
     }
   } catch (e) {
     console.error(`Test failed:`, e);
