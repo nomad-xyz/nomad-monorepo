4452af431a477af92b4bd5b09e6fdd81a648e644
Daniil Naumetc <daniil.naumetc@gmail.com>
2022-02-09 23:33:13 +0100

    fix: die on double update test

--- tools/local-environment/tests/agentsDieOnDoubleUpdate.ts
+++ tools/local-environment/tests/agentsDieOnDoubleUpdate.ts
@@ -20,8 +20,10 @@ import { setupTwo, waitAgentFailure } from "./common";
     const address = new Key().toAddress();
 
     const home = n.getCore(tom).home;
-
     if (!home) throw new Error(`no home`);
+
+    const replica = n.getCore(jerry).getReplica(tom.domain)!;
+    if (!replica) throw new Error(`no replica`);
 
     const xapp = await n.getXAppConnectionManager(jerry);
 
@@ -35,77 +37,23 @@ import { setupTwo, waitAgentFailure } from "./common";
 
     console.log(`Dispatched test transaction to home`);
 
-    const [commitedRoot, newRoot] = await home.suggestUpdate();
+    const [committedRoot] = await home.suggestUpdate();
 
     const updater = await n.getUpdater(tom);
-
-    const { signature } = await updater.signUpdate(commitedRoot, newRoot);
-
-    await (await home.update(commitedRoot, newRoot, signature)).wait();
-
-    console.log(`Submitted valid update to home`);
-
-    const replica = n.getCore(jerry).getReplica(tom.domain)!;
-    if (!replica) throw new Error(`no replica`);
 
     const fraudRoot =
       "0x8bae0a4ab4517a16816ef67120f0e3350d595e014158ba72c3626d8c66b67e53";
 
-    const { signature: fraudletSignature } = await updater.signUpdate(
-      commitedRoot,
+    const { signature: improperSignature } = await updater.signUpdate(
+      committedRoot,
       fraudRoot
     );
+
     await (
-      await replica.update(commitedRoot, fraudRoot, fraudletSignature)
+      await home.update(committedRoot, fraudRoot, improperSignature)
     ).wait();
 
-    console.log(`Submitted fraudulent update to replica`);
-
-    const [homeCommitedRoot, homeRoot, replicaCommitedRoot] = await Promise.all(
-      [home.committedRoot(), home.root(), replica.committedRoot()]
-    );
-
-    if (homeCommitedRoot !== homeRoot)
-      throw new Error(`Home contract's root is not equal to committed root`);
-
-    if (homeRoot === replicaCommitedRoot)
-      throw new Error(
-        `Home contract's root should not be equal to replica's committed root`
-      );
-
-    if (replicaCommitedRoot !== fraudRoot)
-      throw new Error(
-        `Fraud didn't happen: replica's root hasn't been set to fraud root`
-      );
-
-    // Waiting for Home, Replica will fail, and XAppConnectionManager will unenroll the Replica
-    const waiter = new utils.Waiter(
-      async () => {
-        const [homeState, replicaState, domainToReplica, replicaToDomain] =
-          await Promise.all([
-            home.state(),
-            replica.state(),
-
-            xapp.domainToReplica(tom.domain),
-            xapp.replicaToDomain(replica.address),
-          ]);
-
-        if (
-          homeState === 2 && // Waiting till Home state will be failed (2)
-          replicaState === 2 && // Waiting till Replica state will be failed (2)
-          domainToReplica !== replica.address && // Waiting till XAppConnectionManager will stop pointing at replica's address for jerry's domian
-          replicaToDomain !== tom.domain // Waiting till XAppConnectionManager will stop pointing at jerry's domian for replica's address
-        ) {
-          return true;
-        }
-      },
-      3 * 60_000,
-      2_000
-    );
-
-    [, success] = await waiter.wait();
-
-    if (!success) throw new Error(`Fraud was not prevented in time!`);
+    console.log(`Submitted fraud update!`);
 
     let testControlValue: true | undefined, testTimedOut: boolean;
     [testControlValue, testTimedOut] = await updaterWaiter.wait();
