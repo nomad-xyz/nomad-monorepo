c4c4af765a2ee72cf49b9d36bbc1cb08f9f42356
Anna Carroll <anna.s.carroll@gmail.com>
2022-03-02 18:32:28 +0100

    add custom USDC to bridge config

--- typescript/nomad-deploy/config/mainnets/evmos.ts
+++ typescript/nomad-deploy/config/mainnets/evmos.ts
@@ -35,4 +35,13 @@ export const config: CoreConfig = {
 
 export const bridgeConfig: BridgeConfig = {
   weth: 'TODO // WAITING ON CANONICAL WETH DEPLOYMENT',
+  customs: [
+    {
+      id: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+      domain: 0x657468,
+      name: 'USD Coin',
+      symbol: 'USDC',
+      decimals: 6,
+    },
+  ],
 };
